e1297036b989aae462ec4ea2bcfaeeb940701ed2
Ahzam Shahnil <62467164+ahzam-shahnil@users.noreply.github.com>
2021-11-30 01:42:40 +0500

    python Code pratice

---
+++ practice/Date.py
@@ -0,0 +1,20 @@
+class Date:
+    def __init__(self, day, month, year):
+        self.day = day
+        self.month = month
+        self.year = year
+        if not self.validate():
+            raise ValueError("Invalid Date")
+
+    def validate(self):
+        if 1 <= self.day <= 30 and 1 <= self.month <= 12:
+            return True
+        else:
+            return False
+
+    def laterthan(self, other):
+        first = (self.day, self.month, self.year)
+        second = (self.day, self.month, self.year)
+        return first > second
+    # def imcrement_month(self, month):
+    #     sel.
---
+++ practice/Demo Date.py
@@ -0,0 +1,4 @@
+from practice.Date import Date
+
+date = Date(30, 11, 2021);
+print(date.laterthan(Date(1, 12, 2021)))
